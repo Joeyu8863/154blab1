cb0cc5a0326cf10ee92570a313c8b704f024fa3f
JoeYu <57922509+cs130aJoeYu@users.noreply.github.com>
2020-04-12 15:50:41 -0700

    Add files via upload

--- ALU.v
+++ ALU.v
@@ -1,15 +1,21 @@
-//File: alu.v
-//Author: Zhengzhi(Tim) Lu (Individual)
-
-//ALU design as described in the textbook, but with 32 bits instead of 4
-module alu(input [31:0] a, b, input [2:0] f, output [31:0] y, output zero);
-
-wire [31:0] g = f[2]==1'b1 ? ~b : b;
-wire [31:0] k;
-wire c;
-
-assign {c,k} = a+g+f[2];
-assign y = f[1]==1'b1 ? (f[0]==1'b1 ? {31'b0,k[31]} : k) : (f[0]==1'b1 ? a|g : a&g);
-assign zero = ~|y;
-
-endmodule
+module ALU (input [31:0] a, b,input [3:0] f, output reg [31:0] y, output zero) ;
+  wire [31:0] BB ;
+  wire [31:0] S ;
+  wire   cout ;
+  
+  assign BB = (f[3]) ? ~b : b ;
+  assign {cout, S} = f[3] + a + BB ;
+  always @ * begin
+   case (f[2:0]) //add 1bit, addu,subu,xor,sltu,
+    3'b000 : y <= a & BB ;//and /u
+    3'b001 : y <= a | BB ;//or
+    3'b010 : y <= S ;//add /u
+    3'b011 : y <= {31'd0, S[31]};//slt /u
+    3'b001 : y <= a ^ BB ;//xor
+    default: y <= 32'b0;
+   endcase
+  end 
+  
+  assign zero = (y == 0) ;
+   
+ endmodule
